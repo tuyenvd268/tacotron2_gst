938297528e65ce33cdf4e8145cc073438811f433
tuyendv <tuyen.duongvan268@gmail.com>
2023-07-05 20:09:03 +0700

    update

--- ddp_train.py
+++ ddp_train.py
@@ -112,7 +112,8 @@ def save_checkpoint(model, optimizer, step, path):
     
 def load_checkpoint(path, model, optimizer):
     state_dict = torch.load(path, map_location='cpu')
-    model.load_state_dict(state_dict["mode_state_dict"])
+    mode_state_dict = {key.lstrip("module."):value for key, value in state_dict["mode_state_dict"].items()}
+    model.load_state_dict(mode_state_dict)
     optimizer.load_state_dict(state_dict["optimizer_state_dict"])
     step = state_dict["step"]
     # epoch = state_dict["epoch"]
